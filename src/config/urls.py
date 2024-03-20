b03093096f45b4e6dd651a139e9254cc088f65b1
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-03-20 20:02:28 +0100

    HW is done

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from issues.api import create_random_issue, get_issues
+from issues.api import create_random_issue, get_issues, create_poderevyanski_issue, get_poderevyanski_issue
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("issues/", get_issues),
     path("issues/create", create_random_issue),
+    path("issues/oles", create_poderevyanski_issue),
+    path("issues/get-oles", get_poderevyanski_issue)
 ]
--- src/issues/api.py
+++ src/issues/api.py
@@ -1,5 +1,6 @@
 import random
 import string
+import csv
 
 from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render  # noqa
@@ -51,3 +52,55 @@ def create_random_issue(request: HttpRequest) -> JsonResponse:
     }
 
     return JsonResponse(data=result)
+
+
+def create_poderevyanski_issue(request: HttpRequest) -> JsonResponse:
+    path = "/Users/vladimirsaratovsky/Documents/GitHub/hillel_support_project/Oles.csv"
+    data = []
+    result = []
+    num = 1
+
+    with open(path, newline='', encoding='utf-8') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            data.append(row)
+
+    for row in data[1:]:
+        
+        new_title = row[1] + str(num)
+
+        issue = Issue.objects.create(
+        title=new_title,
+        body=row[0],
+        senior_id=2,
+        junior_id=1,
+        )
+
+        result.append({
+            "id": issue.id,
+            "title": issue.title,
+            "body": issue.body,
+            "senior_id": issue.senior_id,
+            "junior_id": issue.junior_id,
+        })
+
+        num += 1
+
+    return JsonResponse(data=result, safe=False)
+
+
+def get_poderevyanski_issue(request: HttpRequest) -> JsonResponse:
+    issues: list[Issue] = Issue.objects.all()
+
+    results: list[dict] = [
+        {
+            "id": issue.id,
+            "title": issue.title,
+            "body": issue.body,
+            "senior_id": issue.senior_id,
+            "junior_id": issue.junior_id,
+        }
+        for issue in issues
+    ]
+
+    return JsonResponse(data={"results": results})
