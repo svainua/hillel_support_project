cd72b8ef59ba53946e82d96e1bcc37a53e7d891b
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-04-06 21:25:52 +0200

    Django models. Migration. Issue entity

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,19 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-8mvhl=94(9opy)=yo4&ng3y=l*cvb6)pv5z#5v&+9$rdz@tn^_"
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,10 +76,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST = 127.0.0.1"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
 }
 
--- src/issues/models.py
+++ src/issues/models.py
@@ -1,11 +1,38 @@
 from django.db import models
+from users.models import User
 
 
 class Issue(models.Model):
-    junior_id = models.IntegerField()
-    senior_id = models.IntegerField()
     title = models.CharField(max_length=100)
-    body = models.TextField()
+    status = models.PositiveSmallIntegerField()
+
+    junior = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="junior_issues"
+    )  # rel_name позволит потом для юзера junior получить все его связанные issues
+    senior = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="senior_issues", null=True
+    )
 
     def __repr__(self) -> str:
         return f"Issue [{self.pk} {self.title[:10]}]"
+
+
+# first_issue: Issue | None = Issue.objects.first()
+# instance: Issue = Issue.objects.get(id=1)
+
+# issue: Issue = Issue.objects.get(id=1)
+# issue.title
+# issue.status
+# issue.junior.password
+
+# #messages = Message.objects.filter(issue_id=issue.id)  вместо этого, при помощи Джанго можно прописать следующее:
+# issue.message_set
+
+
+class Message(models.Model):
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)  автоматич обновл на уровне СУБД,каждый раз,когда вносятся изменения в таблицу
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
