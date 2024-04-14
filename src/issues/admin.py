8d6896aef3b7a2689d0379757a225603796be012
Volodymyr Saratovskyy <sva.in.ua@gmail.com>
2024-04-14 20:06:14 +0200

    HW 18 is done

--- src/issues/admin.py
+++ src/issues/admin.py
@@ -3,11 +3,17 @@ from django.contrib import admin
 from .models import Issue, Message
 
 
+class MessageInline(admin.TabularInline):
+    model = Message
+    extra = 0
+
+
 @admin.register(Issue)
 class IssueAdmin(admin.ModelAdmin):
     list_display = ["id", "title", "junior", "senior"]
+    inlines = [MessageInline]
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ["user", "timestamp", "body"]
+    list_display = ["user", "issue", "timestamp", "body"]
